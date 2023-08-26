3a41cd689ace5a5bf0f0596d17a13dd7f09dc6cc
gabrielts212 <gabrielts2122@gmail.com>
2023-08-26 05:14:38 -0300

    feat: temperatura ✔

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,39 +1,68 @@
-// components/WeatherDisplay.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function WeatherDisplay({ latitude, longitude }) {
-  const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(true);
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
-  useEffect(() => {
-    async function fetchWeather() {
-      try {
-        const apiKey = '7113d500005c402565c5057cfd518400';
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`;
-        const response = await axios.get(url);
-        setWeatherData(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Erro ao obter dados do clima:', error);
-        setLoading(false);
-      }
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=7113d500005c402565c5057cfd518400`;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
     }
-
-    fetchWeather();
-  }, [latitude, longitude]);
-
-  if (loading) {
-    return <p>Carregando...</p>;
   }
 
   return (
-    <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold">{weatherData.name}</h2>
-      <p className="text-lg">{weatherData.main.temp}°C</p>
-      <p className="text-gray-600">{weatherData.weather[0].description}</p>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+  {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+</div>
+
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
+
+      </div>
     </div>
   );
 }
 
-export default WeatherDisplay;
+export default App;
