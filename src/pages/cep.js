02c499d1186d4f55e0b0487b91db74bef5c565f3
gabrielts212 <gabrielts2122@gmail.com>
2023-08-26 03:41:33 -0300

    ✔

--- src/pages/cep.js
+++ src/pages/cep.js
@@ -1,13 +1,17 @@
 
+import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
 import Searchcep from '@/components/Searchcep/searchcep'
 import React from 'react'
 
 const  cep  = () => {
   return (
     <div className="min-h-screen bg-gray-200">
+      <Header/>
     <div className="flex justify-center items-center h-full">
       <Searchcep />
     </div>
+      <Footer/>
   </div>
   )
 }
