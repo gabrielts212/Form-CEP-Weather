f9e92968585ae4d50520c99d0f244a658717ae3e
gabrielts212 <gabrielts2122@gmail.com>
2024-03-07 01:29:52 -0300

    ✔

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="bg-blue-500 text-white py-4">
       <div className="container mx-auto text-center">
         <p>
-          &copy; {new Date().getFullYear()} Meu Site. Todos os direitos
-          reservados.
+          &copy; {new Date().getFullYear()} Gabriel Nascimento. Todos os direitos reservados.
+      
         </p>
       </div>
     </footer>
